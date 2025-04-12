9ae3919708cb43d7a9f011f6e3ea2cdb99bd895a
fangkiigopramana <fangkiigo13@gmail.com>
2025-04-12 22:33:06 +0700

    feat: create contact view

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', fn() => view('index'));
+Route::get('/', fn() => view('home'));
 Route::get('/about', fn()=> 'About')->name('about');
-Route::get('/contact', fn()=> 'Kontakkami')->name('kontakkami');
+Route::get('/contact', fn() => view('kontak.index'));
 Route::get('/home', fn()=> view('home.index'));
 Route::get('/gallery', fn()=> view('Gallery'));
 Route::get('/about', fn()=> view('About'));
