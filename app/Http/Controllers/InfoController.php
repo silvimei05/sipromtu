1e67d0ec83e9ec9d0d4772a9b691e315c617f302
fangkiigopramana <fangkiigo13@gmail.com>
2025-07-08 22:17:01 +0700

    update: ppdb info

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ppdb;
 use Illuminate\Http\Request;
 
 class InfoController extends Controller
@@ -13,6 +14,7 @@ class InfoController extends Controller
      */
     public function ppdb()
     {
-        return view('info_ppdb.index');
+        $infos = Ppdb::all();
+        return view('info_ppdb.index', compact('infos'));
     }
 }
